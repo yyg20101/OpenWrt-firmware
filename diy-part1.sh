466dfcc94e41653184b30917fe3e4b79bb38d343
yyg20101 <250770503@qq.com>
2024-10-15 17:37:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,28 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# Add passwall
+# 定义 Git 仓库地址和 Feed 名称
+REPO_URL="https://github.com/xiaorouji/openwrt-passwall.git"
+FEED_NAME="passwall"
+
+# 获取并过滤最新的 tag，只保留类似 4.78-3 格式的 tag
+LATEST_TAG=$(git ls-remote --tags --sort="v:refname" "$REPO_URL" | awk -F'/' '{print $3}' | grep -E '^[0-9]+\.[0-9]+(-[0-9]+)?$' | tail -n1)
+
+# 判断是否成功获取到最新 tag
+if [ -z "$LATEST_TAG" ]; then
+  echo "未找到符合条件的 tag，使用默认分支"
+  FEED_SOURCE="$REPO_URL"
+else
+  echo "最新的符合条件的 tag 是: $LATEST_TAG"
+  FEED_SOURCE="$REPO_URL^$LATEST_TAG"
+fi
+
+# 添加 feed source 到 feeds.conf.default
+echo "src-git $FEED_NAME $FEED_SOURCE" >> feeds.conf.default
+
+echo "已将 $FEED_NAME feed source 添加到 feeds.conf.default"
+
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git^4.78-3' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git^4.78-3' >>feeds.conf.default
