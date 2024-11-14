1606aa48b275ff117606e0b9d04626ab8d97c53b
yyg20101 <250770503@qq.com>
2024-11-14 21:28:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,35 +11,35 @@
 #
 
 # Uncomment a feed source
-sed -i '/helloworld/d' feeds.conf.default
-sed -i '/luci/d' feeds.conf.default
+#sed -i '/helloworld/d' feeds.conf.default
+#sed -i '/luci/d' feeds.conf.default
 
 # Add luci
-sed -i '2isrc-git luci https://github.com/yyg20101/luci.git;openwrt-23.05' feeds.conf.default
+#sed -i '2isrc-git luci https://github.com/yyg20101/luci.git;openwrt-23.05' feeds.conf.default
 
 # Add passwall
 # 定义 Git 仓库地址和 Feed 名称
-REPO_URL="https://github.com/xiaorouji/openwrt-passwall.git"
-FEED_NAME="passwall"
+#REPO_URL="https://github.com/xiaorouji/openwrt-passwall.git"
+#FEED_NAME="passwall"
+#
+## 获取并过滤最新的 tag，只保留类似 4.78-3 格式的 tag
+#LATEST_TAG=$(git ls-remote --tags --sort="v:refname" "$REPO_URL" | awk -F'/' '{print $3}' | grep -E '^[0-9]+\.[0-9]+(-[0-9]+)?$' | tail -n1)
+## 指定tag版本
+##LATEST_TAG="4.78-2"
 
-# 获取并过滤最新的 tag，只保留类似 4.78-3 格式的 tag
-LATEST_TAG=$(git ls-remote --tags --sort="v:refname" "$REPO_URL" | awk -F'/' '{print $3}' | grep -E '^[0-9]+\.[0-9]+(-[0-9]+)?$' | tail -n1)
-# 指定tag版本
-#LATEST_TAG="4.78-2"
-
-# 判断是否成功获取到最新 tag
-if [ -z "$LATEST_TAG" ]; then
-  echo "未找到符合条件的 tag，使用默认分支"
-  FEED_SOURCE="$REPO_URL"
-else
-  echo "最新的符合条件的 tag 是: $LATEST_TAG"
-  FEED_SOURCE="$REPO_URL^$LATEST_TAG"
-fi
-
-# 添加 feed source 到 feeds.conf.default
-echo "src-git $FEED_NAME $FEED_SOURCE" >> feeds.conf.default
-
-echo "已将 $FEED_NAME feed source 添加到 feeds.conf.default"
-
+## 判断是否成功获取到最新 tag
+#if [ -z "$LATEST_TAG" ]; then
+#  echo "未找到符合条件的 tag，使用默认分支"
+#  FEED_SOURCE="$REPO_URL"
+#else
+#  echo "最新的符合条件的 tag 是: $LATEST_TAG"
+#  FEED_SOURCE="$REPO_URL^$LATEST_TAG"
+#fi
+#
+## 添加 feed source 到 feeds.conf.default
+#echo "src-git $FEED_NAME $FEED_SOURCE" >> feeds.conf.default
+#
+#echo "已将 $FEED_NAME feed source 添加到 feeds.conf.default"
+#
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
