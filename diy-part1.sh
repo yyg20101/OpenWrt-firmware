7691e9ef794b49ef7511efc0697e424651755b26
yyg20101 <250770503@qq.com>
2025-02-03 16:08:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -52,4 +52,7 @@ if [ -f "$PROJECT_MIRRORS_FILE" ]; then
     sed -i '/.cn\//d; /tencent/d; /aliyun/d' "$PROJECT_MIRRORS_FILE"
 fi
 
+# 修改开源站地址
 sed -i '/.cn\//d; /tencent/d; /aliyun/d' scripts/download.pl
+# sed -i 's/mirror.iscas.ac.cn/mirrors.mit.edu/g' scripts/download.pl
+# sed -i 's/mirrors.aliyun.com/mirror.netcologne.de/g' scripts/download.pl
